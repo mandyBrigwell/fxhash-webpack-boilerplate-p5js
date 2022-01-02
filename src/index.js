fd6bd39843437f1a39c73d31b38336b313c894f5
mandyBrigwell <69047092+mandyBrigwell@users.noreply.github.com>
2022-01-02 07:32:35 +0000

    Update index.js
    Setup for my preferences.

--- src/index.js
+++ src/index.js
@@ -1,63 +1,33 @@
 import p5 from "p5";
 
-// these are the variables you can use as inputs to your algorithms
-console.log(fxhash)   // the 64 chars hex number fed to your algorithm
-console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
-const seed = ~~(fxrand()*123456789);
-let s;
-
-const numCircles = ~~(fxrand()*500) + 100;
+const randomSeed = ~~(fxrand()*12345);
+const noiseSeed = ~~(fxrand()*56789);
+let screenSize;
 
 window.$fxhashFeatures = {
-  "Density": numCircles > 500?"High":(numCircles<200?"Low":"Medium")
+	"Feature": 0
 }
-
-// note about the fxrand() function 
-// when the "fxhash" is always the same, it will generate the same sequence of
-// pseudo random numbers, always
-
-//----------------------
-// defining features
-//----------------------
-// You can define some token features by populating the $fxhashFeatures property
-// of the window object.
-// More about it in the guide, section features:
-// [https://fxhash.xyz/articles/guide-mint-generative-token#features]
-//
-// window.$fxhashFeatures = {
-//   "Background": "Black",
-//   "Number of lines": 10,
-//   "Inverted": true
-// }
-
-// this code writes the values to the DOM as an example
 
 let sketch = function(p5) {
 
-  p5.setup = function() {
-    p5.noLoop();
-    s = p5.min(p5.windowWidth, p5.windowHeight);
-    p5.createCanvas(s, s);
-  };
+	p5.setup = function() {
+		p5.randomSeed(randomSeed);
+		p5.noiseSeed(noiseSeed);
+		p5.colorMode(p5.HSB, 360);
+		screenSize = p5.min(p5.windowWidth, p5.windowHeight);
+		p5.createCanvas(screenSize, screenSize);
+		};
 
-  p5.draw = function() {
-    p5.randomSeed(seed);
-    p5.background("#FFD");
-    p5.push();
-    for (var i = numCircles; i >= 0; i--) {
-      let c = p5.color(p5.random(["#50B", "#313", "#713"]));
-      c.setAlpha(13);
-      p5.fill(c);
-      p5.noStroke();
-      p5.circle(p5.random()*p5.width, p5.random()*p5.height, p5.abs(p5.randomGaussian(0, p5.height/20)));
-    }
-    p5.pop();
-  };
+	p5.draw = function() {
+		p5.background(128);
+		p5.translate(screenSize/2, screenSize/2);
+		p5.ellipse(0, 0, 64, 64);
+	};
 
 	p5.windowResized = function() {
 		if (navigator.userAgent.indexOf("HeadlessChrome") == -1) {
-			s = p5.min(p5.windowWidth, p5.windowHeight);
-			p5.resizeCanvas(s, s);
+			screenSize = p5.min(p5.windowWidth, p5.windowHeight);
+			p5.resizeCanvas(screenSize, screenSize);
 			p5.initiate();
 		}
 	}
